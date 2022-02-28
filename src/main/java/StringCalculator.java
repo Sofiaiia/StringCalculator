786e4eaaacb84244f794d260e1639a9b419f37ea
sofia södergårdh <sofia.s@aland.net>
2022-02-28 17:15:53 +0200

    punkt 1 klar

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,6 +1,17 @@
 public class StringCalculator {
 
     public int add(String numbers){
+        if(numbers.isEmpty()){
+            return 0;
+        }else if(numbers.contains(",")){
+            String[] array = numbers.split(",",2);
+            return toInteger(array[0]) + toInteger(array[1]);
+        }else{
+            return toInteger(numbers);
+        }
+    }
 
+    private int toInteger(String s){
+        return Integer.parseInt(s);
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,9 +1,31 @@
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorTest {
 
     @Test
-    public void startTest(){
+    public void EmptyString(){
+        String s = "";
+        StringCalculator calc = new StringCalculator();
+        int empty = calc.add(s);
+        Assertions.assertEquals(0,empty);
+    }
 
+    @Test
+    public void oneNumber(){
+        String s = "1";
+        StringCalculator calc = new StringCalculator();
+        int one = calc.add(s);
+        Assertions.assertEquals(1,one);
+    }
+
+    @Test
+    public void twoNumbers(){
+        String s = "1,2";
+        StringCalculator calc = new StringCalculator();
+        int two = calc.add(s);
+        Assertions.assertEquals(3,two);
     }
 }
